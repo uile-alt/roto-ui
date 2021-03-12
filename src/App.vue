6ef02403667922c1a4ca238172e21a83a5760d67
uile-alt <haotian908@gmail.com>
2021-03-12 23:34:49 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <HelloWorld></HelloWorld>
+  <Demo></Demo>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Demo from './components/Demo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Demo
+  }
+}
+</script>
---
+++ src/components/Demo.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>demo</h1>
+</template>
+<script>
+    export default {
+      name: 'Demo'
+    }
+</script>
