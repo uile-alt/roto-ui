5a898f66a640ba8c792f5bd74414e0661033119a
uile-alt <haotian908@gmail.com>
2021-03-13 22:48:25 +0800

    添加 aside 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,8 @@
     <div class="banner">
       <h1>RotoUI</h1>
       <p class="actions">
-        <a href="">Github</a>
-        <a href="">开始</a>
+        <a href="https://github.com/uile-alt/roto-ui">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -21,4 +21,26 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
+</style>
