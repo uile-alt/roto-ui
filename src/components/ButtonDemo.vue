5b8338165641ec289546ec5127ba9389f8ddf5f4
uile-alt <haotian908@gmail.com>
2021-03-16 22:57:22 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,11 @@
 <template>
   <div>Button的文档</div>
+  <Button>hi</Button>
 </template>
 <script lang='ts'>
+import Button from "../lib/Button.vue";
 export default {
   name: "ButtonDemo",
+  components: { Button },
 };
 </script>
