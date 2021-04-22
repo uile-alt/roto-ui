46eb3589ff0157802812bccc8126d55e8eb1b0c9
uile-alt <haotian908@gmail.com>
2021-04-22 22:45:29 +0800

    支持自定义 title 和 content

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,26 +1,32 @@
 <template>
   <div>示例</div>
   <Button @click="toggle">toggle</Button>
-  <Dialog v-model:visible="x" :close-on-click-overlay="true"
-  :ok="f1" :cancel="f2"></Dialog>
+  <Dialog
+    v-model:visible="x"
+    :close-on-click-overlay="true"
+    :ok="f1"
+    :cancel="f2">
+    <template v-slot:title>实例</template>
+    <template v-slot:content><div>内容</div></template>
+  </Dialog>
 </template>
 <script lang='ts'>
-  import Dialog from "../lib/Dialog.vue";
-  import Button from "../lib/Button.vue";
-  import {ref} from 'vue'
+import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
 export default {
-  components:{Dialog,Button},
-  setup(){
-    const x = ref(false)
-    const toggle = ()=>{
-      x.value = !x.value
-    }
-    const f1 = ()=>{
+  components: { Dialog, Button },
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+      x.value = !x.value;
+    };
+    const f1 = () => {
       console.log(1);
-      return true
-    }
-    const f2 = ()=>{}
-    return {x, toggle,f1,f2}
-  }
+      return true;
+    };
+    const f2 = () => {};
+    return { x, toggle, f1, f2 };
+  },
 };
 </script>
