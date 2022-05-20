5c4c0eb42f742a2528e007f0a39f919a1b0728db
uile-alt <haotian908@gmail.com>
2022-05-20 21:37:15 +0800

    优化代码

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -7,7 +7,7 @@
         :ref="
           (el) => {
             //@ts-ignore
-            if (el) navItems[index] = el;
+            if (t === selected) selectedItem = el;
           }
         "
         @click="select(t)"
@@ -33,28 +33,25 @@
 
 <script setup lang="ts">
 import Tab from "./Tab.vue";
-import { useSlots, ref, computed, onMounted, onUpdated } from "vue";
+import { useSlots, ref, computed, watchEffect, onMounted } from "vue";
 const props = defineProps<{
   selected: string;
 }>();
 
 const defaults = useSlots().default();
-const navItems = ref<HTMLDivElement[]>([]);
+const selectedItem = ref<HTMLDivElement>(null);
 const indicator = ref<HTMLDivElement>(null);
 const container = ref<HTMLDivElement>(null);
-const x = () => {
-  const divs = navItems.value;
-  const result = divs.filter((div) => div.classList.contains("selected"))[0];
-  const { width } = result.getBoundingClientRect();
-  indicator.value.style.width = width + "px";
-  const { left: left1 } = container.value.getBoundingClientRect();
-  const { left: left2 } = result.getBoundingClientRect();
-  const left = left2 - left1;
-  indicator.value.style.left = left + "px";
-  console.log(result);
-};
-onMounted(x);
-onUpdated(x);
+onMounted(() => {
+  watchEffect(() => {
+    const { width, left: left2 } = selectedItem.value.getBoundingClientRect();
+    indicator.value.style.width = width + "px";
+    const { left: left1 } = container.value.getBoundingClientRect();
+    const left = left2 - left1;
+    indicator.value.style.left = left + "px";
+  });
+});
+
 defaults.forEach((tag) => {
   if (tag.type !== Tab) {
     throw new Error("Tabs 子标签必须为 Tab");
@@ -65,11 +62,6 @@ const emit = defineEmits(["update:selected"]);
 function select(title: string) {
   emit("update:selected", title);
 }
-const current = computed(() => {
-  return defaults.filter((tag) => {
-    return tag.props.title === props.selected;
-  })[0];
-});
 
 const titles = defaults.map((tag) => {
   return tag.props.title;
